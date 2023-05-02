a00509d5f2208c773a837194ce2244857b30e4b8
Schiller, Thomas (STUDENTS) <thomas.schiller@students.unibe.ch>
2023-05-02 17:32:41 +0200

    shortened the upscaling to two methods

--- Image.py
+++ Image.py
@@ -63,33 +63,18 @@ class Image():
     #   y_new: Interpolated 1D signal, numpy array
         x_vals = np.linspace(1,len(signal),len(signal))
         x_new = np.linspace(1,len(signal), int(len(signal)*scale_factor))
-        y_new = self.__interp(signal, x_vals, x_new)
-        return y_new
-
-    def __interp(self, y_vals, x_vals, x_new):
-    # Computes interpolation at the given abscissas
-    #
-    # Inputs:
-    #   x_vals: Given inputs abscissas, numpy array
-    #   y_vals: Given input ordinates, numpy array
-    #   x_new : New abscissas to find the respective interpolated ordinates, numpy
-    #   arrays
-    #
-    # Outputs:
-    #   y_new: Interpolated values, numpy array
         y_new = np.zeros(len(x_new))
         for i in range(len(x_new)):
             if (x_new[i] <= x_vals[0]):
-                y_new[i] = y_vals[0]
+                y_new[i] = signal[0]
             elif (x_new[i] >= x_vals[len(x_vals)-1]):
-                y_new[i] = y_vals[len(y_vals)-1]
+                y_new[i] = signal[len(signal)-1]
             elif (x_new[i]-np.floor(x_new[i]) < 0.00000001):
-                y_new[i] = y_vals[x_vals == int(x_new[i])]
+                y_new[i] = signal[x_vals == int(x_new[i])]
             else:
                 x0 = int(np.floor(x_new[i])) 
                 x1 = int(np.ceil(x_new[i])) 
-                fx0 = y_vals[x0-1]
-                fx1 = y_vals[x1-1]
+                fx0 = signal[x0-1]
+                fx1 = signal[x1-1]
                 y_new[i] = fx0 *(1- (x_new[i] - x0) / (x1 - x0)) + fx1 * (x_new[i] - x0) / (x1 - x0)
-
         return y_new
